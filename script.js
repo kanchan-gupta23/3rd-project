4995f54246d5572d3d0b0bf422dc48b7c376bbbb
kanchan-gupta23 <kanchangupta23102004@gmail.com>
2024-03-30 17:12:38 +0530

    #commit

---
+++ script.js
@@ -0,0 +1,26 @@
+gsap.to(".vdo",{
+    scrollTrigger:{
+        trigger:".home",
+        pin:true,
+        scrub: 2,
+        start:"top top",
+        end:"bottom top"
+    },
+    '--clip': '0%',
+    duration: 1,
+    ease: Power2
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
